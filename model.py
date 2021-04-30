8373e1cb50a49a97ced74a7fb8a272cecb3dbb3e
connorlarson <67910506+connorlarson@users.noreply.github.com>
2021-04-30 16:57:09 -0400

    added carbon box model

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ rd.seed(69420)
 # number of days to simulate
 N = 365
 
-# rainfall metrics from Google
+# rainfall metrics from Google (imperial units)
 yearly_rainfall = 47  # average yearly rainfall in inches
 rainy_days = 102  # average rainy days per year
 DAYS_IN_YEAR = 365
@@ -64,7 +64,7 @@ def plot_rain(rainfall):
 
 # soil moisture box model setup
 # parameters (all quantities normalized per unit area)
-Z = 12  # depth of the soil in inches
+Z = 32  # depth of the soil in inches
 n = 0.5  # porosity
 s_h = 0.1  # hygroscopic point
 s_w = 0.2  # wilting point
@@ -124,7 +124,7 @@ def plot_water_loss():
 # %%
 
 # forward euler time step for soil moisture
-def sim_euler(rainfall, s_init=s_h):
+def sim_soil_moisture(rainfall, s_init=s_h):
     s = s_init
     series = []
 
@@ -160,7 +160,7 @@ def f_d(s):
 # function to plot soil moisture dependence
 def plot_moisture_dependence():
     x_axis = [i / 100 for i in range(100)]
-    y_axis = [f_d(i/100) for i in range(100)]
+    y_axis = [f_d(i / 100) for i in range(100)]
     plt.plot(x_axis, y_axis)
     plt.title("Dependence of Decomposition on Soil Moisture")
     plt.xlabel("Relative Soil Moisture")
@@ -170,7 +170,89 @@ def plot_moisture_dependence():
 
 
 # %%
+# carbon box model setup (metric units)
+
+# parameters
+ADD = 1.5  # litterfall in gC per day*m^2
+k_d = 8.5 * 10 ** (-3)
+k_l = 6.5 * 10 ** (-5)
+k_h = 2.5 * 10 ** (-6)
+r_h = 0.25
+r_r = 0.75
+
+# initial values (porporato et al.)
+C_h_init = 8500
+C_b_init = 80
+C_l_init = 1200
+
+
+# functions
+def BD(C_l, C_h, C_b, s):
+    return k_d * C_b
+
+
+def DEC_l(C_l, C_h, C_b, s):
+    return f_d(s) * k_l * C_b * C_l
+
+
+def DEC_h(C_l, C_h, C_b, s):
+    return f_d(s) * k_h * C_b * C_h
+
+
+def dC_l(C_l, C_h, C_b, s):
+    return ADD + BD(C_l, C_h, C_b, s) - DEC_l(C_l, C_h, C_b, s)
+
+
+def dC_h(C_l, C_h, C_b, s):
+    return r_h * DEC_l(C_l, C_h, C_b, s) + DEC_h(C_l, C_h, C_b, s)
+
+
+def dC_b(C_l, C_h, C_b, s):
+    return (1 - r_h - r_r) * DEC_l(C_l, C_h, C_b, s) + (1 - r_r) * DEC_h(C_l, C_h, C_b, s) - BD(C_l, C_h, C_b, s)
+
+
+# euler box model for carbon
+def sim_carbon(s, C_l=C_l_init, C_h=C_h_init, C_b=C_b_init):
+    # time series for carbon
+    C_l_series = []
+    C_h_series = []
+    C_b_series = []
+
+    for t in range(len(s)):
+        d = [dC_l(C_l, C_h, C_b, s[t]), dC_h(C_l, C_h, C_b, s[t]), dC_b(C_l, C_h, C_b, s[t])]
+
+        C_l += d[0]
+        C_h += d[1]
+        C_b += d[2]
+
+        C_l_series.append(C_l)
+        C_h_series.append(C_h)
+        C_b_series.append(C_b)
+
+    return {"Litter": C_l_series, "Humus": C_h_series, "Biomass": C_b_series}
+
+
+# plot carbon levels
+def plot_carbon(C):
+    for box in C:
+        y = C[box]
+        x = range(len(y))
+        plt.plot(x, y, label=box)
+
+    plt.title("Carbon in Pools")
+    plt.xlabel("Day")
+    plt.ylabel("Carbon (gC / (m^3)")
+    plt.legend()
+    plt.show()
+    plt.close()
+
+
+# %%
 # type code to run below (or in console)
-# rain = sim_rain(90)
-# plot_rain(rain)
-# plot_soil_moisture(sim_euler(rain))
+h = sim_rain()
+s = sim_soil_moisture(h)
+C = sim_carbon(s)
+
+plot_rain(h)
+plot_soil_moisture(s)
+plot_carbon(C)
